20b33ce8a49e271924a7cc99145263e8a8d907af
aubbiali <aubbiali@sorint.it>
2021-09-22 19:08:44 +0200

    Delete unused comments

--- bikeProject/src/main/java/bikeProject/Main.java
+++ bikeProject/src/main/java/bikeProject/Main.java
@@ -21,17 +21,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("ARRIVA");
         try {
-            // Se vuoi usare la cartella panels nel path di bikeProject devi mettere gli fxml li e usare queste
-            // stringhe:
-            /*URL url = Paths.get("src/main/java/bikeProject/panels/loginPanel.fxml").toUri().toURL();
-            FXMLLoader loader = new FXMLLoader(url);*/
-            // getResource punta alle resources
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("homePagePanel.fxml"));
-            System.out.println("PROBLEMA");
             Parent root = loader.load();
-            System.out.println("qua");
 
             Scene scene = new Scene(root);
             stage.setScene(scene);
@@ -39,33 +32,6 @@ public class Main extends Application {
         } catch ( Exception e ) {
             e.printStackTrace();
         }
-
-//		// to mantain the state throught different panels
-//
-//		FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("/bikeProject/panels/loginPanel.fxml"));
-//		Parent firstPane = firstPaneLoader.load();
-//		Scene firstScene = new Scene(firstPane);
-//
-////		// getting loader and a pane for the second scene
-////		FXMLLoader secondPageLoader = new FXMLLoader(getClass().getResource("/bikeProject/panels/provaPanel
-// .fxml"));
-////		Parent secondPane = secondPageLoader.load();
-////		Scene secondScene = new Scene(secondPane);
-//
-//		// injecting second scene into the controller of the first scene
-//		bikeProject.controller.Controller firstPaneController = (bikeProject.controller.Controller) firstPaneLoader
-//				.getController();
-//		firstPaneController.setSecondScene(secondScene);
-//
-//		// injecting first scene into the controller of the second scene
-//		bikeProject.controller.ControllerProva secondPaneController = (bikeProject.controller.ControllerProva)
-//		secondPageLoader
-//				.getController();
-//		secondPaneController.setFirstScene(firstScene);
-//
-//		primaryStage.setTitle("Switching scenes");
-//		primaryStage.setScene(firstScene);
-//		primaryStage.show();
 
     }
 
@@ -78,24 +44,5 @@ public class Main extends Application {
             System.out.println(e);
         }
     }
-
-    /*module org.openjfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.openjfx to javafx.fxml;
-    exports org.openjfx;
-    }*/
-
-    /*requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires json.simple;*/
-
-    /*opens org.openjfx to javafx.fxml;
-    exports org.openjfx;*/
-    // opens bikeProject to javafx.fxml;
-    //opens bikeProject.controller to javafx.fxml;
-    //exports bikeProject;
 
 }
--- bikeProject/src/main/java/bikeProject/database/Database.java
+++ bikeProject/src/main/java/bikeProject/database/Database.java
@@ -5,24 +5,25 @@ import java.sql.DriverManager;
 
 public class Database implements Database_Interface {
 
-	public Connection conn;
+    public Connection conn;
 
-	public Database() {
-		createConnection();
-	}
+    public Database() {
+        createConnection();
+    }
 
-	public Connection getConnection() {
-		return this.conn;
-	}
+    public Connection getConnection() {
+        return this.conn;
+    }
 
-	public void createConnection() {
-		try {
-			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bikeProject?user=bikeProject&password" +
-					"=8bikeProject8*");
-		} catch ( Exception e ) {
-			e.printStackTrace();
-		}
-		System.out.println("Connected to db: bikeProject");
-	}
+    public void createConnection() {
+        try {
+            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bikeProject?user=bikeProject" +
+					"&password" + "=8bikeProject8*");
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Connected to db: bikeProject");
+    }
 
 }
