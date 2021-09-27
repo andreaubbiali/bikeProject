3fca6a3ce53ec3084871e09ca4fea8578b65544a
aubbiali <aubbiali@sorint.it>
2021-09-27 17:29:40 +0200

    Update rack position database logic

--- bikeProject/src/main/java/bikeProject/database/RackPositionDatabase.java
+++ bikeProject/src/main/java/bikeProject/database/RackPositionDatabase.java
@@ -1,16 +1,26 @@
 package bikeProject.database;
 
-import bikeProject.dataservice.BikeType;
-
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class RackPositionDatabase implements RackPositionDatabaseInterface {
 
-    public void updateIsBroken(boolean isBroken) throws SQLException {
+    public void updateIsBroken(long ID, boolean isBroken) throws SQLException {
 
+        PreparedStatement statement = Database.getConn().prepareStatement("UPDATE rack_position SET is_broken = ? " +
+                "WHERE id = ?; ");
+
+        statement.setBoolean(1, isBroken);
+        statement.setLong(2, ID);
+
+        // execute the query
+        int res = statement.executeUpdate();
+
+        if ( res != 1 ) {
+            throw new SQLException();
+        }
+
+        statement.close();
     }
 
     public void addRackPositions(long rackID, int numberPositions, String bikeType) throws SQLException {
@@ -31,5 +41,20 @@ public class RackPositionDatabase implements RackPositionDatabaseInterface {
 
         }
 
+    }
+
+    public void addBike(long rackPositionID, long bikeID) throws SQLException {
+
+        // prepare the statement
+        PreparedStatement statement = Database.getConn().prepareStatement("UPDATE rack_position SET bike_id = ? " +
+                "WHERE id = ?");
+        statement.setLong(1, bikeID);
+        statement.setObject(2, rackPositionID);
+
+        // execute the query
+        int res = statement.executeUpdate();
+        if ( res == 0 ) {
+            throw new SQLException("Add bike failed, no rows affected.");
+        }
     }
 }
--- bikeProject/src/main/java/bikeProject/database/RackPositionDatabaseInterface.java
+++ bikeProject/src/main/java/bikeProject/database/RackPositionDatabaseInterface.java
@@ -3,7 +3,9 @@ package bikeProject.database;
 import java.sql.SQLException;
 
 public interface RackPositionDatabaseInterface {
-    void updateIsBroken(boolean isBroken) throws SQLException;
+    void updateIsBroken(long ID, boolean isBroken) throws SQLException;
 
     void addRackPositions(long rackID, int numberPositions, String bikeType) throws SQLException;
+
+    void addBike(long rackPositionID, long bikeID) throws SQLException;
 }
