eba813bf7c8b003e67521f7db6b10c2dd2a240b1
aubbiali <aubbiali@sorint.it>
2021-09-27 17:29:21 +0200

    Update bike database logic

--- bikeProject/src/main/java/bikeProject/database/BikeDatabase.java
+++ bikeProject/src/main/java/bikeProject/database/BikeDatabase.java
@@ -2,15 +2,14 @@ package bikeProject.database;
 
 import bikeProject.dataservice.Bike;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class BikeDatabase implements BikeDatabaseInterface {
 
     public void updateBike(Bike bike) throws SQLException {
 
-        PreparedStatement statement = Database.getConn().prepareStatement("UPDATE bike(is_in_maintenance) VALUES(?) " +
-                "WHERE id = ?; ");
+        PreparedStatement statement =
+                Database.getConn().prepareStatement("UPDATE bike(is_in_maintenance) VALUES(?) " + "WHERE id = ?; ");
 
         statement.setBoolean(1, bike.isInMaintenance());
         statement.setLong(2, bike.getID());
@@ -22,5 +21,39 @@ public class BikeDatabase implements BikeDatabaseInterface {
 
         statement.close();
 
+    }
+
+    public long createBike(long bikeTypeID) throws SQLException {
+
+        PreparedStatement statement = Database.getConn().prepareStatement("INSERT INTO bike (type_id) VALUES (?);",
+                Statement.RETURN_GENERATED_KEYS);
+        statement.setLong(1, bikeTypeID);
+
+        // execute the query
+        statement.execute();
+
+        // get the id
+        try ( ResultSet generatedKeys = statement.getGeneratedKeys() ) {
+            if ( generatedKeys.next() ) {
+                return generatedKeys.getLong(1);
+            } else {
+                throw new SQLException("Register bike failed, no ID obtained.");
+            }
+        }
+    }
+
+    public void delete(long bikeID) throws SQLException {
+
+        PreparedStatement statement = Database.getConn().prepareStatement("DELETE FROM bike WHERE id = ?");
+        statement.setLong(1, bikeID);
+
+        // execute query
+        int res = statement.executeUpdate();
+
+        if ( res != 1 ) {
+            throw new SQLException("Cannot delete bike");
+        }
+
+        statement.close();
     }
 }
--- bikeProject/src/main/java/bikeProject/database/BikeDatabaseInterface.java
+++ bikeProject/src/main/java/bikeProject/database/BikeDatabaseInterface.java
@@ -7,4 +7,8 @@ import java.sql.SQLException;
 public interface BikeDatabaseInterface {
 
     void updateBike(Bike bike) throws SQLException;
+
+    long createBike(long bikeTypeID) throws SQLException;
+
+    void delete(long bikeID) throws SQLException;
 }
